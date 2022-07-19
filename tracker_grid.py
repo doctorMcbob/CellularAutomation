6c820dd4b8d2bcbe7352a10e55236194d8d93d81
doctorMcbob <wesley@abe.ai>
2022-07-19 02:22:33 -0700

    tracker

--- tracker_grid.py
+++ tracker_grid.py
@@ -1,47 +1,15 @@
 """
-lemme just throw a docstring up here
+So. Here we are again.
 
-keys:
-  RETURN   |   type number for rule
-  SPACE    |   play, pause
-  r        |   random rule
-  c        |   clear (reset board)
-  s        |   save rule
+I was looking at the directional coloring scheme and thinking it would 
+be cool if i could scope it to only look at specific colors
+Reminder, the colors are picked by the 8 cell code a cell is currently representing
+this means we can look at individual colors and see how cells in a specific permutation
+are being 'used' across the automata ruleset. this is interesting! maybe could be used to
+intuit what is happening in the rule to a better degree?
 
-soon ill put in drawing cells and customizing rules
-
-
-essentially the same as the linear cellular automata,
-but instead of the 3 cells above, its the 8 cells surrounding
-
-rules are a binary number between 0 and 2^256
-
-neighboring cells are converted to binary
-
-0 0 1
-1   0
-0 1 1
-
-becomes 00110011 becomes 51
-
-then the 51(th) index of the binary rule becomes the cells value in the next generation
-
-
-2^2^8 or 2^256 possible rules (thats a lot!)
-
-TO DO LIST
-[x] save interesting rules to file
-[x] load interesting
-[x] mouse functionality for drawing
-[x] rule "builder"
-... all 256 possible permutations visualised
-    in a GUI so you can set the exact rule you want
-
-REDUX; i want a display on screen that shows the rule visualised at all times, make the grid smaller to fit it
-then you can alter the rule as it goes easily
-
-rule loader as well
-
+anyways thats the idea. ive been drinking and its passed midnight, lets get into it.
+Happy birthday.
 """
 from random import randint
 
@@ -74,6 +42,10 @@ num = 0
 
 timer = 300 if "-t" not in sys.argv else int(sys.argv[sys.argv.index("-t") + 1])
 
+SHOW_COLOR = True
+
+PERMUTATIONS = {}
+SCOPE = []
 STACK = []
 FIRST_DUP = None
 
@@ -85,6 +57,8 @@ BTN_POSITIONS = {
     "horiz": ((PW*86, PW*35), (PW*4, PW*2)),
     "vert": ((PW*91, PW*35), (PW*4, PW*2)),
     "record": ((PW*96, PW*35), (PW*4, PW*2)),
+    "color": ((PW*103, PW*35), (PW*4, PW*2)),
+    "clear": ((PW*113, PW*35), (PW*4, PW*2)),
 }
 
 def pack(grid): return "".join(["".join(row) for row in grid])
@@ -95,19 +69,20 @@ def rotate_8bit(slot): return "".join([slot[i] for i in [5, 3, 0, 6, 1, 7, 4, 2]
 def flip_8bit_horiz(slot): return "".join([slot[i] for i in [2, 1, 0, 4, 3, 7, 6, 5]])
 def flip_8bit_vert(slot): return "".join([slot[i] for i in [5, 6, 7, 3, 4, 0, 1, 2]])
 
-def color(slot, state):
+def color2(slot, state):
     r = int("".join([slot[i] for i in [5, 3, 0, 1, 2]]), 2) * 8
     g = int("".join([slot[i] for i in [1, 2, 4, 7, 6]]), 2) * 8
     b = int("".join([slot[i] for i in [7, 6, 5, 3, 0]]), 2) * 8
 
+    """
     if state == 1:
         r = (r + 128) % 255
         g = (g + 128) % 255
         b = (b + 128) % 255
-
+    """
     return (255-r, 255-g, 255-b)
 
-def color2(slot, state, normalize=False):
+def color(slot, state, normalize=False):
     density = sum([int(n) for n in slot])
     thrd = 8 / 3
     r = 20 + (density               ) * (256 / 8)
@@ -155,11 +130,42 @@ def apply_rule(rule, grid):
     return new
 
 def drawn_grid(grid):
+    # im doing it here because fuck it
+    PERMUTATIONS.clear()
+    
     surf = Surface((W * PW, H * PW))
+    surf.fill((255, 255, 255))
     for y, line in enumerate(grid):
         for x, slot in enumerate(line):
-            pygame.draw.rect(surf, (50, 50, 50), Rect((x*PW, y*PW), (PW, PW)))
-            pygame.draw.rect(surf, color(nbrs((x, y), grid), slot), Rect((x*PW+1, y*PW+1), (PW-2, PW-2)))
+            col = color(nbrs((x, y), grid), slot)
+            PERMUTATIONS[col] = 1 if col not in PERMUTATIONS else PERMUTATIONS[col] + 1
+                    
+            if SHOW_COLOR and (not SCOPE or col in SCOPE):
+                pygame.draw.rect(surf, (50, 50, 50), Rect((x*PW, y*PW), (PW, PW)))
+                pygame.draw.rect(surf, col, Rect((x*PW+1, y*PW+1), (PW-2, PW-2)))
+            if not SHOW_COLOR:
+                pygame.draw.rect(surf, (50, 50, 50), Rect((x*PW, y*PW), (PW, PW)))
+                pygame.draw.rect(surf, (0, 0, 0) if int(slot) else (255, 255, 255), Rect((x*PW+1, y*PW+1), (PW-2, PW-2)))
+    return surf
+
+def drawn_permutations(idx=None):
+    surf = Surface((80, len(PERMUTATIONS.keys()) * 32))
+    surf.fill((255, 255, 255))
+
+    values = list((v, k) for k, v in PERMUTATIONS.items())
+    values.sort()
+    color_by_occurence = [k for v, k in values[::-1]]
+
+    for i, key in enumerate(color_by_occurence):
+        if idx is not None and idx == i:
+            if any(pygame.mouse.get_pressed()):
+                if key not in SCOPE:
+                    SCOPE.append(key)
+            pygame.draw.rect(surf, (100, 255, 100), Rect((PW, i*PW), (64-PW, PW)))
+        if key in SCOPE:
+            pygame.draw.rect(surf, (180, 255, 180), Rect((PW, i*PW), (64-PW, PW)))
+        pygame.draw.rect(surf, key, Rect((0, i*PW), (PW, PW)))
+        surf.blit(HEL.render(str(PERMUTATIONS[key]), 0, (0, 0, 0)), (PW+2, i*PW))
     return surf
 
 def draw_buttons(dest, btn_positions=BTN_POSITIONS):
@@ -167,7 +173,9 @@ def draw_buttons(dest, btn_positions=BTN_POSITIONS):
         "vert": VERT_SYM,
         "horiz": HORIZ_SYM,
         "rotation": ROT_SYM,
-        "record": RECORD_MODE
+        "record": RECORD_MODE,
+        "color": SHOW_COLOR,
+        "clear": not SCOPE,
     }
     for name in btn_positions:
         pos, dim = btn_positions[name]
@@ -179,21 +187,23 @@ def drawn_mini(grid):
     surf = Surface((W * 8, H * 8))
     for y, line in enumerate(grid):
         for x, slot in enumerate(line):
-            pygame.draw.rect(surf, color2(nbrs((x, y), grid), slot), Rect((x*8, y*8), (8, 8)))
+            pygame.draw.rect(surf, color(nbrs((x, y), grid), slot), Rect((x*8, y*8), (8, 8)))
     return surf
 
 def drawn_rule_seg(bit, on=False):
-    surf = Surface((PW * 1.5, PW * 1.5))
-    surf.fill((255, 255, 255))
+    surf = Surface((PW * 1.5 + 4, PW * 1.5 + 4))
+    c = color(bit, 0)
+    surf.fill((255, 0, 0) if c in SCOPE else (255, 255, 255))
     x, y = (0, 2)
+    
     for i in bit:
-        pygame.draw.rect(surf, (50, 50, 50), Rect((x*(PW//2), y*(PW//2)), (PW//2, PW//2)))
+        pygame.draw.rect(surf, (50, 50, 50), Rect((2+x*(PW//2), 2+y*(PW//2)), (PW//2, PW//2)))
         if (x, y) == (1, 1):
-            pygame.draw.rect(surf, [(255, 255, 255), (50, 150, 50)][int(on)], Rect((x*(PW//2)+1, y*(PW//2)+1), ((PW//2)-2, (PW//2)-2)))
+            pygame.draw.rect(surf, [(255, 255, 255), (50, 150, 50)][int(on)], Rect((2+x*(PW//2)+1, 2+y*(PW//2)+1), ((PW//2)-2, (PW//2)-2)))
             x += 1
-            pygame.draw.rect(surf, (50, 50, 50), Rect((x*(PW//2), y*(PW//2)), (PW//2, PW//2)))
+            pygame.draw.rect(surf, (50, 50, 50), Rect((2+x*(PW//2), 2+y*(PW//2)), (PW//2, PW//2)))
 
-        pygame.draw.rect(surf, col[int(i)], Rect((x*(PW//2)+1, y*(PW//2)+1), ((PW//2)-2, (PW//2)-2)))
+        pygame.draw.rect(surf, col[int(i)], Rect((2+x*(PW//2)+1, 2+y*(PW//2)+1), ((PW//2)-2, (PW//2)-2)))
 
         x += 1
         if x == 3:
@@ -220,7 +230,7 @@ def drawn_rule(rule):
     return surf
 
 def btn_click(btn_positions=BTN_POSITIONS):
-    global ROT_SYM, HORIZ_SYM, VERT_SYM, RECORD_MODE
+    global ROT_SYM, HORIZ_SYM, VERT_SYM, RECORD_MODE, SHOW_COLOR, SCOPE
     pygame.display.update()
     x, y = pygame.mouse.get_pos()
 
@@ -230,6 +240,8 @@ def btn_click(btn_positions=BTN_POSITIONS):
             if name == "horiz": HORIZ_SYM = not HORIZ_SYM
             if name == "vert": VERT_SYM = not VERT_SYM
             if name == "record": RECORD_MODE = not RECORD_MODE
+            if name == "color": SHOW_COLOR = not SHOW_COLOR
+            if name == "clear": SCOPE = []
             
 
 def rule_click(corner=(PW*80, PW*2)):
@@ -248,6 +260,13 @@ def rule_click(corner=(PW*80, PW*2)):
     y = y // 4
 
     value = x + (y*16)
+
+    if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+        if color(tobin(value, 8), 0) not in SCOPE:
+            SCOPE.append(color(tobin(value, 8), 0))
+        else:
+            SCOPE.remove(color(tobin(value, 8), 0))
+        return
     
     if rule[value] == "0":
         num += 2 ** value
@@ -386,8 +405,19 @@ while live:
     SCREEN.fill((255, 255, 255))
     if str(num) in savelist: SCREEN.blit(HEL.render("Rule " + str(num) + " (S)", 0, (0, 0, 0)), (0, 0))
     else: SCREEN.blit(HEL.render("Rule " + str(num), 0, (0, 0, 0)), (0, 0))
+
+    mpos = pygame.mouse.get_pos()
+    idx = None
+    if Rect((PW*70, PW * 2), (80, len(PERMUTATIONS.keys()) * 32)).collidepoint(mpos):
+        x, y = mpos
+        y -= PW * 2
+        idx = y // PW
+
+    SCREEN.blit(drawn_permutations(idx), (PW*70, PW * 2))
     SCREEN.blit(drawn_grid(grid), (PW * 2, PW * 2))
     SCREEN.blit(drawn_rule(num), (PW * 80, PW * 2))
+
+
     if FIRST_DUP:
         SCREEN.blit(HEL.render("First Repeating: frame " + str(FIRST_DUP[0]) + " repeats on frame " + str(FIRST_DUP[1]), 0, (0, 0, 0)), (PW*80, PW * 56))
     SCREEN.blit(HEL.render("Frame: " + str(len(STACK)), 0, (0, 0, 0)), (PW*80, PW * 55))
@@ -398,8 +428,8 @@ while live:
         t += CLOCK.tick(30)
         if t > timer:
             if "-c" in sys.argv:
-                if not os.path.isdir("colorpics/"+str(num)): os.mkdir("colorpics/"+str(num))
-                pygame.image.save(drawn_mini(grid), "colorpics/"+str(num)+"/"+str(len(STACK))+".png")
+                if not os.path.isdir("trackerpics/"+str(num)): os.mkdir("trackerpics/"+str(num))
+                pygame.image.save(drawn_mini(grid), "trackerpics/"+str(num)+"/"+str(len(STACK))+".png")
             t = 0
             layer = pack(grid)
             if layer in STACK and not FIRST_DUP:
